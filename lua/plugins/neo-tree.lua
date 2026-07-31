1c327aaa0518bd0cda7b96b2eb17138d5c3ef46f
TselmuunUranchimeg <tselmuunu@gmail.com>
2026-07-31 14:41:35 +0800

    Fixed a neo-tree invisible popup text bug

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,22 +5,23 @@ vim.pack.add {
 }
 
 vim.keymap.set('n', '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
-vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'NeoTree toggle', silent = true } )
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'NeoTree toggle', silent = true })
 
-require('neo-tree').setup({
+require('neo-tree').setup {
+  popup_border_style = 'rounded',
   filesystem = {
     filtered_items = {
-        hide_dotfiles = false,
+      hide_dotfiles = false,
+    },
+    window = {
+      mappings = {
+        ['\\'] = 'close_window',
+        ['<leader>e'] = 'close_window',
       },
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-          ['<leader>e'] = 'close_window',
-        },
-      },
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = false,
-      },
+    },
+    follow_current_file = {
+      enabled = true,
+      leave_dirs_open = false,
+    },
   },
-})
+}
