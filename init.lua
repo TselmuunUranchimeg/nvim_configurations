8c388793167a2c3fcfddc3ec3300c1cb9014a19f
TselmuunUranchimeg <tselmuunu@gmail.com>
2026-09-12 19:45:27 +0800

    Added git-blame for inline-blame annotations

--- init.lua
+++ init.lua
@@ -229,6 +229,14 @@ do
   vim.pack.add { 'https://github.com/OXY2DEV/markview.nvim' }
 end
 
+-- Inline-blame annotations
+do
+  vim.pack.add { 'https://github.com/f-person/git-blame.nvim' }
+  require('gitblame').setup {
+    enabled = true,
+  }
+end
+
 do
   vim.pack.add {
     {
